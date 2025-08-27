4bcb5d43538c757ebf035838825071b7045f0043
Juan_uni <juan.uni@doublevpartners.com>
2025-08-27 17:19:54 -0500

    primera prueba

--- resource_group/provider.tf
+++ resource_group/provider.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.104.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  subscription_id            = var.subscription_id
+  tenant_id                  = var.tenant_id
+  skip_provider_registration = true
 }
+

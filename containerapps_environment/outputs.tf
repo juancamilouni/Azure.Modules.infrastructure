a8a1d30aeb8c09ef9162cdb0f6b9816d9d43236e
Juan_uni <juan.uni@doublevpartners.com>
2025-09-09 08:16:24 -0500

    aca

--- containerapps_environment/outputs.tf
+++ containerapps_environment/outputs.tf
@@ -7,8 +7,3 @@ output "aca_environment_default_domain" {
   description = "Dominio interno por defecto del Environment"
   value       = azurerm_container_app_environment.this.default_domain
 }
-
-output "aca_environment_static_ip" {
-  description = "IP estática del Environment (si aplica)"
-  value       = try(azurerm_container_app_environment.this.static_ip, null)
-}

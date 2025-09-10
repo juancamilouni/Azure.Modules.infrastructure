fb4fc085eff2ca4abec8a5360bd84372cd93195d
Juan_uni <juan.uni@doublevpartners.com>
2025-09-10 08:45:04 -0500

    ajuste key

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -3,24 +3,19 @@ resource "azurerm_key_vault" "kv" {
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = var.tenant_id
-  sku_name            = "standard"
 
-  soft_delete_retention_days = 30
-  purge_protection_enabled   = true
+  # ← Usa las variables declaradas
+  sku_name                  = var.sku_name
+  soft_delete_retention_days = var.soft_delete_retention_days
+  purge_protection_enabled   = var.purge_protection_enabled
 
-  #  Nombre correcto en azurerm 3.x
-  enable_rbac_authorization = true
+  # Nombre correcto del atributo en el provider azurerm 3.x
+  enable_rbac_authorization  = var.rbac_authorization_enabled
 
-  # (Opcional y soportado en 3.x+)
-  public_network_access_enabled = true
+  public_network_access_enabled = var.public_network_access_enabled
 
-  # 🔕 Quita los access_policy si usas RBAC
-  # access_policy { ... }  # <-- eliminar
+  # No mezclar RBAC con access_policy
+  # access_policy { ... }  # ← eliminar si usas RBAC
 
   tags = var.tags
-
-  # (Opcional) algunos habilitan esto porque Azure a veces “toca” el flag:
-  # lifecycle {
-  #   ignore_changes = [ enable_rbac_authorization ]
-  # }
 }

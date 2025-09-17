72d61e914cbe7c235e749590bf155ba6679584d8
Juan_uni <juan.uni@doublevpartners.com>
2025-09-17 06:14:27 -0500

    ajuste

--- application_gateway/main.tf
+++ application_gateway/main.tf
@@ -1,29 +1,16 @@
-##########################################
-# Public IP (creada automáticamente)
-##########################################
-resource "azurerm_public_ip" "this" {
-  name                = "${var.name}-pip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  allocation_method = "Static"
-  sku               = "Standard"
-
-  tags = var.tags
-}
-
-##########################################
-# Application Gateway
-##########################################
 resource "azurerm_application_gateway" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
 
   sku {
-    name     = "WAF_Medium" # Calidad-precio: WAF v1
-    tier     = "WAF"
-    capacity = var.capacity
+    name = "WAF_v2"
+    tier = "WAF_v2"
+  }
+
+  autoscale_configuration {
+    min_capacity = 1
+    max_capacity = 3
   }
 
   gateway_ip_configuration {
@@ -46,78 +33,5 @@ resource "azurerm_application_gateway" "this" {
     public_ip_address_id = azurerm_public_ip.this.id
   }
 
-  ##########################################
-  # Listeners
-  ##########################################
-  http_listener {
-    name                           = "listener-web"
-    frontend_ip_configuration_name = "frontend-ip"
-    frontend_port_name             = "https-port"
-    host_name                      = var.web_domain
-    protocol                       = "Https"
-    ssl_certificate_name           = var.ssl_cert != null ? "ssl-cert" : null
-  }
-
-  http_listener {
-    name                           = "listener-api"
-    frontend_ip_configuration_name = "frontend-ip"
-    frontend_port_name             = "https-port"
-    host_name                      = var.api_domain
-    protocol                       = "Https"
-    ssl_certificate_name           = var.ssl_cert != null ? "ssl-cert" : null
-  }
-
-  ##########################################
-  # Certificado SSL (opcional)
-  ##########################################
-  dynamic "ssl_certificate" {
-    for_each = var.ssl_cert != null ? [1] : []
-    content {
-      name     = "ssl-cert"
-      data     = var.ssl_cert.data
-      password = var.ssl_cert.password
-    }
-  }
-
-  ##########################################
-  # Backends
-  ##########################################
-  backend_address_pool {
-    name  = "backend-web"
-    fqdns = [var.swa_fqdn]
-  }
-
-  backend_address_pool {
-    name  = "backend-api"
-    fqdns = [var.apim_fqdn]
-  }
-
-  backend_http_settings {
-    name                  = "http-settings"
-    cookie_based_affinity = "Disabled"
-    port                  = 443
-    protocol              = "Https"
-    request_timeout       = 60
-  }
-
-  ##########################################
-  # Rules
-  ##########################################
-  request_routing_rule {
-    name                       = "rule-web"
-    rule_type                  = "Basic"
-    http_listener_name         = "listener-web"
-    backend_address_pool_name  = "backend-web"
-    backend_http_settings_name = "http-settings"
-  }
-
-  request_routing_rule {
-    name                       = "rule-api"
-    rule_type                  = "Basic"
-    http_listener_name         = "listener-api"
-    backend_address_pool_name  = "backend-api"
-    backend_http_settings_name = "http-settings"
-  }
-
-  tags = var.tags
+  # listeners, backends, rules...
 }

cc00a073f93606c7746312c451383e6a8e0d1013
Juan_uni <juan.uni@doublevpartners.com>
2025-09-17 10:54:16 -0500

    ajuste

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -2,16 +2,16 @@
 # Key Vault
 ##########################################
 resource "azurerm_key_vault" "this" {
-  name                        = var.name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = var.tenant_id
 
   sku_name                   = var.sku_name
   soft_delete_retention_days = var.soft_delete_retention_days
   purge_protection_enabled   = var.purge_protection_enabled
 
-  enable_rbac_authorization   = var.rbac_authorization_enabled
+  enable_rbac_authorization     = var.rbac_authorization_enabled
   public_network_access_enabled = var.public_network_access_enabled
 
   network_acls {
